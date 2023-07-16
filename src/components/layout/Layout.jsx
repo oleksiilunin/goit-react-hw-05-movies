10cef08f324568acb89cd9a9ed575ce057fd4c09
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-16 13:50:41 +0300

    Add GlobalStyles  to Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,28 +1,32 @@
+import { GlobalStyles } from 'components/GlobalStyles';
 import { Suspense } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/movies">Movies</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Outlet />
-        </Suspense>
-      </main>
-      <footer>Footer</footer>
-    </div>
+    <>
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/movies">Movies</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </main>
+        <footer>Footer</footer>
+      </div>
+      <GlobalStyles />
+    </>
   );
 };
 
