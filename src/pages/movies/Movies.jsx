854c3dbe6bc8e87ff76ba96d3704801d05369487
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-17 21:00:21 +0300

    Create structure  for  Movies

--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -1,58 +1,101 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { get } from 'services/api';
+import { toast } from 'react-toastify';
+import notifyOptions from 'helpers/toastNotifyOptions';
+import { FiSearch } from 'react-icons/fi';
 
 const Movies = () => {
-  const [movies, setMovies] = useState([
-    'movie-1',
-    'movie-2',
-    'movie-3',
-    'movie-4',
-    'movie-5',
-    'movie-6',
-  ]);
-
   const location = useLocation();
+  const [page, setPage] = useState(1);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [searchData, setSearchData] = useState(null);
+  const previousInputValue = useRef('');
 
-  const movieId = searchParams.get('movieId') ?? '';
+  const movieName = searchParams.get('movieName') ?? '';
 
-  // useEffect(() => {
-  //   return () => {};
-  // }, []);
-  const updateQueryString = e => {
-    const movieIdValue = e.target.value;
-    if (movieIdValue === '') {
-      return setSearchParams({});
+  const ENDPOINT = `/search/movie?query=${movieName}`;
+
+  console.log('MOVIE_NAME >>>', movieName);
+
+  const fetchSearchingMovies = async () => {
+    try {
+      const data = await get(ENDPOINT, page);
+      console.log(data.results);
+      setSearchData(data);
+    } catch (error) {
+      console.error('Помилка при виконанні запиту:', error);
     }
-    setSearchParams({ movieId: movieIdValue });
-    // const nextParams = movieId !== '' ? { movieId } : {};
-    // setSearchParams(nextParams);
   };
 
-  // TODO: для фильтрации использовать useMemo. ???
+  useEffect(() => {
+    if (movieName) {
+      fetchSearchingMovies();
+    }
 
-  const visibleMovies = movies.filter(movie =>
-    movie.toLowerCase().includes(movieId.toLowerCase())
-  );
-  console.log(location);
+    // return () => {
+    //   cancelAllRequests(); // Скасовує всі запити при розмонтуванні компонента
+    // };
+  }, []);
+
+  const updateQueryString = e => {
+    const movieNameValue = e.target.value;
+    if (movieNameValue === '') {
+      return setSearchParams({});
+    }
+    setSearchParams({ movieName: movieNameValue.trim() });
+  };
+
+  const handleSearchMovie = event => {
+    event.preventDefault();
+    const currentInputValue = event.target.query.value;
+    console.log('currentInputValue>>>>>>', currentInputValue);
+
+    if (movieName === '') {
+      toast.info('Enter a search query', notifyOptions);
+      return;
+    }
+    if (currentInputValue === previousInputValue.current) {
+      return;
+    }
+
+    fetchSearchingMovies();
+
+    previousInputValue.current = currentInputValue;
+  };
 
   return (
     <div>
-      Movies
-      <input type="text" value={movieId} onChange={updateQueryString} />
-      <button onClick={() => setSearchParams({ h: 'hello' })}>
-        Change search params
-      </button>
+      <form onSubmit={handleSearchMovie}>
+        <input
+          type="text"
+          name="query"
+          value={movieName}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search movies"
+          onChange={updateQueryString}
+        />
+
+        <button type="submit">
+          <FiSearch size={24} />
+          Search
+        </button>
+      </form>
+
       <ul>
-        {visibleMovies.map(movie => {
-          return (
-            <li key={movie}>
-              <Link to={`${movie}`} state={{ from: location }}>
-                {movie}
-              </Link>
-            </li>
-          );
-        })}
+        {searchData &&
+          searchData.results.map(({ id, title }) => {
+            return (
+              title && (
+                <li key={id}>
+                  <Link to={`/movie/${id}`} state={{ from: location }}>
+                    {title}
+                  </Link>
+                </li>
+              )
+            );
+          })}
       </ul>
     </div>
   );
