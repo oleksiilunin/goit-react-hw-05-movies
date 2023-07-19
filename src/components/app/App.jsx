a0a7f78714e79479a50c595277e4eba65e95bbc6
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-19 23:43:10 +0300

    Delete commented code

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../layout/Layout';
 import { lazy } from 'react';
-// import Home from 'pages/Home';
-// import Movies from 'pages/Movies';
-// import MovieDetails from 'pages/MovieDetails';
-// import Description from './Description';
-// import Screenshots from './Screenshots';
 
 const Home = lazy(() => import('../../pages/home/Home'));
 const Movies = lazy(() => import('../../pages/movies/Movies'));
@@ -14,11 +9,6 @@ const MovieDetails = lazy(() =>
 );
 const Reviews = lazy(() => import('../reviews/Reviews'));
 const Cast = lazy(() => import('../cast/Cast'));
-//Для того, чтобы работала ленивая загрузка(lazy) c модулем с именованным импортом, то необходимо делать как показано ниже
-// const Screenshots = lazy(() => import('./Screenshots')).then({
-//   ...module,
-//   default: module.Screenshots,
-// });
 
 export default function App() {
   return (
