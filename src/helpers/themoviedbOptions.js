55cb9362247e5214d31c2c124e06acf1b3dffcb5
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-21 14:08:03 +0300

    fix

--- src/helpers/themoviedbOptions.js
+++
@@ -1,6 +0,0 @@
-const BASE_URL = ' https://api.themoviedb.org/3';
-// const API_KEY = 'f829c1853a6e4b429c6a90c6139f439c';
-const BEARER_TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODI5YzE4NTNhNmU0YjQyOWM2YTkwYzYxMzlmNDM5YyIsInN1YiI6IjY0YWQ1YzVlOGEwZTliMDBlMzc1M2VmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5WH9ltQhhGnfZ0snarPEFwBogRn3ds5bNR0L46BO2lw';
-
-export { BASE_URL, BEARER_TOKEN };
