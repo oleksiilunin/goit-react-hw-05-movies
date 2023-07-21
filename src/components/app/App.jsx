434667384b1146d5801b1646183fb84d8a988208
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-21 14:20:40 +0300

    fix

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,28 +1,27 @@
-// import { Route, Routes } from 'react-router-dom';
-// import Layout from '../layout/Layout';
-// import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from '../layout/Layout';
+import { lazy } from 'react';
 
-// const Home = lazy(() => import('../../pages/home/Home'));
-// const Movies = lazy(() => import('../../pages/movies/Movies'));
-// const MovieDetails = lazy(() =>
-//   import('../../pages/movieDetails/MovieDetails')
-// );
-// const Reviews = lazy(() => import('../reviews/Reviews'));
-// const Cast = lazy(() => import('../cast/Cast'));
+const Home = lazy(() => import('../../pages/home/Home'));
+const Movies = lazy(() => import('../../pages/movies/Movies'));
+const MovieDetails = lazy(() =>
+  import('../../pages/movieDetails/MovieDetails')
+);
+const Reviews = lazy(() => import('../reviews/Reviews'));
+const Cast = lazy(() => import('../cast/Cast'));
 
 export default function App() {
   return (
-    <div>App</div>
-    // <Routes>
-    //   <Route path="/" element={<Layout />}>
-    //     <Route index element={<Home />} />
-    //     <Route path="movie" element={<Movies />} />
-    //     <Route path="movie/:movieId" element={<MovieDetails />}>
-    //       <Route path="reviews" element={<Reviews />} />
-    //       <Route path="cast" element={<Cast />} />
-    //     </Route>
-    //     {/* <Route path="*" element={<NotFound />} /> */}
-    //   </Route>
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="movie" element={<Movies />} />
+        <Route path="movie/:movieId" element={<MovieDetails />}>
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="cast" element={<Cast />} />
+        </Route>
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Route>
+    </Routes>
   );
 }
